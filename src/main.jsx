73f1369975504fcc5615b06fb28e528f6787ae2b
jamil <jamilujibrilla01@gmail.com>
2022-11-15 13:23:48 -0800

    fix: import error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './Router'
 import './index.css'
-import ScrollToTop from "./scrollToTop";
+import ScrollToTop from "./ScrollToTop";
 import {BrowserRouter} from "react-router-dom"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
